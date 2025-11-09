3852916ca6751abf62eed9e0ee9ee4d5dfa82d33
Artemie <jernovoi.artemie@gmail.com>
2025-11-09 22:37:50 +0200

    Lab04: Jenkins + SSH agent + PHP app + tests + Jenkinsfile

---
+++ lab04/php-app/index.php
@@ -0,0 +1,3 @@
+﻿<?php
+function add($a, $b) { return $a + $b; }
+echo "Hello! 2+3=" . add(2,3) . PHP_EOL;
---
+++ lab04/tests/AddTest.php
@@ -0,0 +1,9 @@
+﻿<?php
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../php-app/index.php';
+
+final class AddTest extends TestCase {
+  public function testAdd(): void {
+    $this->assertSame(5, add(2,3));
+  }
+}
